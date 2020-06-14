1e61222a22192adc13fda0bbb20912229296d8cf
Talrevivo <37411352+Talrevivo@users.noreply.github.com>
2020-06-14 13:54:26 +0300

    Add files via upload

---
+++ BoundProject/jScripts/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $("#menu-img").click(function () {
+        $(".nav-design").show();
+        $("#x").show();
+        $(".nav-design").addClass("navclick-design");
+    });
+
+    $("#x").click(function () {
+        $(".nav-design").hide();
+        $(".nav-design").addClass("navclick-design");
+        location.reload();
+    });
+});
